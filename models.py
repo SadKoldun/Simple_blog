cbb2f77544b9f05ca49dbbd9da49453c7399a0bc
SadKoldun <serezhashostak9@gmail.com>
2023-12-12 12:24:55 +0200

    Improving app structure

---
+++ models.py
@@ -0,0 +1,47 @@
+from typing import List
+
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy import ForeignKey, String, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy()
+
+
+class BlogPost(Base):
+    __tablename__ = "blog_posts"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title = mapped_column(String(250), unique=True, nullable=False)
+    subtitle = mapped_column(String(250), nullable=False)
+    date = mapped_column(String(250), nullable=False)
+    body = mapped_column(Text, nullable=False)
+    author_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    author: Mapped["User"] = relationship(back_populates="post")
+    img_url = mapped_column(String(250), nullable=False)
+    comments_from_post: Mapped[List["Comment"]] = relationship(back_populates="post_with_comment", cascade="all, delete")
+
+
+class User(UserMixin, Base):
+    __tablename__ = "user"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email = mapped_column(String(250), unique=True, nullable=False)
+    password = mapped_column(String(250), nullable=False)
+    name = mapped_column(String(250), nullable=False)
+    post: Mapped[List["BlogPost"]] = relationship(back_populates="author")
+    user_comments: Mapped[List['Comment']] = relationship(back_populates="comment_author")
+
+
+class Comment(Base):
+    __tablename__ = "comments"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    text = mapped_column(String(250), unique=True, nullable=False)
+    comment_author_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    comment_author: Mapped["User"] = relationship(back_populates="user_comments")
+    post_with_comment_id: Mapped[int] = mapped_column(ForeignKey("blog_posts.id"))
+    post_with_comment: Mapped["BlogPost"] = relationship(back_populates="comments_from_post")
---
+++ post/view.py
@@ -0,0 +1,74 @@
+from datetime import date
+from flask import render_template, redirect, url_for, Blueprint
+from flask_login import current_user
+from post.post_forms import CreatePostForm, CommentForm
+from models import Comment, BlogPost, db
+from utils import admin_only
+
+post_blueprint = Blueprint('post_blueprint', __name__, template_folder='templates')
+
+
+@post_blueprint.route("/post/<int:post_id>", methods=["GET", "POST"])
+def show_post(post_id):
+    requested_post = db.get_or_404(BlogPost, post_id)
+    comment_form = CommentForm()
+    if comment_form.validate_on_submit():
+        new_comment = Comment(
+            text=comment_form.comment_text.data,
+            comment_author=current_user,
+            post_with_comment=requested_post
+        )
+        db.session.add(new_comment)
+        db.session.commit()
+
+    return render_template("post.html", post=requested_post, form=comment_form)
+
+
+@post_blueprint.route("/new-post", methods=["GET", "POST"])
+@admin_only
+def add_new_post():
+    form = CreatePostForm()
+    if form.validate_on_submit():
+        new_post = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            body=form.body.data,
+            img_url=form.img_url.data,
+            author=current_user,
+            date=date.today().strftime("%B %d, %Y")
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
+    return render_template("make-post.html", form=form)
+
+
+@post_blueprint.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+@admin_only
+def edit_post(post_id):
+    post = db.get_or_404(BlogPost, post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = current_user
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post.id))
+    return render_template("make-post.html", form=edit_form, is_edit=True)
+
+
+@post_blueprint.route("/delete/<int:post_id>")
+@admin_only
+def delete_post(post_id):
+    post_to_delete = db.get_or_404(BlogPost, post_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
